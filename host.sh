334f434fa68119c0d0fd558b42d8c3dcf4635e44
disconnect3d <dominik.b.czarnota@gmail.com>
2017-11-11 02:02:13 +0100

    Move container building to host.sh

--- host.sh
+++ host.sh
@@ -3,5 +3,6 @@
 PORT=4444
 IMAGENAME=test
 
+docker build -t $IMAGENAME .
 socat tcp-l:$PORT,reuseaddr,fork exec:"./spawn_container.sh $IMAGENAME"
 
--- spawn_container.sh
+++ spawn_container.sh
@@ -2,5 +2,4 @@
 
 IMAGENAME=$1
 
-docker build -t $IMAGENAME .
 docker run --rm -i -v `pwd`/tasks:/tasks:ro -u jailed $IMAGENAME bash
