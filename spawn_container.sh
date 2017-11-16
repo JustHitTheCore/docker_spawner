91e2a8904f4b000598e4a2e318e04bd3be86dfcd
disconnect3d <dominik.b.czarnota@gmail.com>
2017-11-16 14:18:55 +0100

    Add missing -t to get tty in docker run

--- spawn_container.sh
+++ spawn_container.sh
@@ -7,7 +7,7 @@ read password
 
 if [ "$password" == "$PASSWORD" ]; then
     echo "Good pass, spawning docker container for you!"
-    docker run --rm -i -v `pwd`/tasks:/tasks:ro -u jailed $IMAGENAME bash
+    docker run --rm -it -v `pwd`/tasks:/tasks:ro -u jailed $IMAGENAME bash
 else
     echo "Wrong password!"
 fi
