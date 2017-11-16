3da3396d58b241005008db022aa4ad7dda022621
disconnect3d <dominik.b.czarnota@gmail.com>
2017-11-16 14:09:10 +0100

    echo

--- host.sh
+++ host.sh
@@ -3,6 +3,9 @@
 PORT=4444
 IMAGENAME=ctf_workshops
 
+echo "Building docker image $IMAGENAME"
 docker build -t $IMAGENAME .
+
+echo "Hosting socat on port $PORT"
 socat tcp-l:$PORT,reuseaddr,fork exec:"./spawn_container.sh $IMAGENAME"
 
